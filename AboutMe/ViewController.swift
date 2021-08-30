0850c6b945efbb749dcfba735026818c59126e34
vpark777 <vkphottie@gmail.com>
2021-08-30 14:07:51 -0700

    Added a comment to AboutMeViewController

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -1,14 +1,14 @@
 //
 //  ViewController.swift
 //  AboutMe
-//
+//  First Project for Hanko Academy by Victoria Park
 //  Created by Hannie Kim on 8/1/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
