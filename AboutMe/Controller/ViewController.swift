37dd45af16adce3869f2bc42528ce44c0cc3f62d
Yani <bayanijulian@gmail.com>
2021-09-01 09:00:32 -0500

    wip dynamically update constraints

--- AboutMe/Controller/ViewController.swift
+++ AboutMe/Controller/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    
+    let blueBox: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    override func loadView() {
+//        super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .blue
+        self.view = UIView()
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(blueBox)
+        layoutBlueBox()
+
+        NSLayoutConstraint.activate([
+            blueBox.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            blueBox.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor)
+        ])
+       
+//        NSLayoutConstraint.
+        
     }
+    
+    private func layoutBlueBox() {
+        print("layoutBlueBox called")
+        var displayHeight: NSLayoutDimension = self.view.safeAreaLayoutGuide.heightAnchor
+        var displayWidth: NSLayoutDimension = self.view.safeAreaLayoutGuide.widthAnchor
+        
+//        let height = self.view.safeAreaLayoutGuide.heightAnchor.value(forKey: )
+        
+        let shouldNormalizeDisplaySize = UIDevice.current.orientation.isLandscape
+        if (shouldNormalizeDisplaySize) {
+            displayWidth = self.view.safeAreaLayoutGuide.heightAnchor
+            displayHeight = self.view.safeAreaLayoutGuide.widthAnchor
+        }
+        print(self.view.safeAreaLayoutGuide.heightAnchor)
+        print(self.view.safeAreaLayoutGuide.widthAnchor)
+        NSLayoutConstraint.deactivate([blueBox.heightAnchor.constraint(equalTo: displayHeight, multiplier: 1/5),
+                                       blueBox.widthAnchor.constraint(equalTo: displayWidth, multiplier: 1/3)])
+        NSLayoutConstraint.activate([
+            blueBox.heightAnchor.constraint(equalTo: displayHeight, multiplier: 1/5),
+            blueBox.widthAnchor.constraint(equalTo: displayWidth, multiplier: 1/3)
+        ])
+    }
+    
+    override func viewWillLayoutSubviews() {
+        layoutBlueBox()
+    }
+    
 
 }
 
