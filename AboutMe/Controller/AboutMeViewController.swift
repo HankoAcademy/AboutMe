17bf04d8324cb839f9fcdfe83850e15567daffec
Diana Ruiz-Olvera <dp.ruizolvera@gmail.com>
2021-09-13 15:08:05 -0400

    Week 1 Work Completed

---
+++ AboutMe/Controller/AboutMeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Hannie Kim on 8/1/21.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+    
+    override func loadView() {
+        view = DetailView()
+        view.backgroundColor = .white
+        
+    }
+
+}
